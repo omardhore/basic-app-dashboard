2229decba29dd6d392ea98e95a8b39de678c0667
omardhoor <omardhoore80@gmail.com>
2026-01-20 12:26:06 +0300

    Redirect root to login page

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', [AdminController::class, 'index'])->middleware(['auth', 'twofactor', 'verified'])->name('dashboard');
