ba3f891a992661a81dd4f71bec11745d7f36dcb9
omardhoor <omardhoore80@gmail.com>
2026-01-20 17:16:17 +0300

    Enable 2FA by default for new users

--- database/migrations/2026_01_19_142026_add_two_factor_enabled_to_users_table.php
+++ database/migrations/2026_01_19_142026_add_two_factor_enabled_to_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('two_factor_enabled')->default(false);
+            $table->boolean('two_factor_enabled')->default(true);
         });
     }
 
